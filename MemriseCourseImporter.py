99b9d24224adf732e2edac3d14a6d70bf1a768c0
Dominik Wild <dominik.wild@gmail.com>
2014-04-12 16:03:06 +0200

    changed to unix line endings

--- MemriseCourseImporter.py
+++ MemriseCourseImporter.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-# Memrise Course Importer
+﻿# -*- coding: utf-8 -*-# Memrise Course Importer
 # Copyright 2013 Pete Schlette <pete.schlette@gmail.com>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
@@ -9,4 +9,4 @@ This add-on adds an item labelled "Import Memrise Course..." to the Tools menu
 in Anki that allows you to download and import a course from 
 Memrise as a deck (or, more precisely, a series of sub-decks).
 """
-import Memrise_Course_Importer.importer
+import Memrise_Course_Importer.importer
